0f53f5d9b37ae31b87f9f408ec27b998e89e69a8
Jonathan A. McCormick, Jr <67705789+LiberlandHacker@users.noreply.github.com>
2023-02-06 22:02:09 -0600

    Auto-assign dark mode

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
 from flask import Flask, render_template
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
+app.debug = True
 
+# DARK MODE AUTO-ASSIGN
+
+def get_mode():
+    
+
+    def is_nighttime():
+        new_hampshire_tz = pytz.timezone('America/New_York')
+        now = datetime.now(new_hampshire_tz)
+        current_time_nh = int(now.strftime("%H%M%S"))
+        return not 60000 < current_time_nh < 180000  # Use IRL
+        #return 60000 < current_time_nh < 180000  # Use ONLY for testing
+
+    if is_nighttime():
+        return 'dark'
+    else:
+        return 'light'
+
+
+# Pages
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    mode = get_mode()
+    return render_template('index.html', mode=mode)
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    mode = get_mode()
+    return render_template("about.html", mode=mode)
 
 @app.route("/analytics")
 def analytics():
-    return render_template("analytics.html")
+    mode = get_mode()
+    return render_template("analytics.html", mode=mode)
 
 @app.route("/survey")
 def survey():
+    mode = get_mode()
     chores = [
         "Cleaning the kitchen",
         "Vacuuming or sweeping floors",
@@ -34,9 +60,8 @@ def survey():
         "Cleaning the car(s)",
         "Pet care"
     ]
-    return render_template('survey.html', chores=chores)
+    return render_template('survey.html', mode=mode, chores=chores)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81)
-
